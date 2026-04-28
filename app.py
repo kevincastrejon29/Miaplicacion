d194cd790a47e330cac4cdb36ef692e20058230c
Kevin <kevincastrejonsosa29@gmail.com>
2026-04-28 00:20:23 -0500

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import numpy as np
 import libreria_funciones as lf
 
-st.image("logo.png", width=100)
-st.sidebar.image("Logo DMC.png") 
+#st.image("logo.png", width=100)
+#st.sidebar.image("Logo DMC.png") 
 
 
 st.title("Mi primera aplicación")
